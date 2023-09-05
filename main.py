8a255fa530ad3e4bf58ce9362cb640357659f45d
jsommer6 <juliansommer2107@gmail.com>
2023-09-05 13:13:22 +1000

    actually working

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 WIKIPEDIA_MAX_QUERY_LENGTH = 300
 matplotlib.use("agg")
 
+
 def create_folder_if_not_exists(folder_path):
     if not os.path.exists(folder_path):
         try:
@@ -39,17 +40,15 @@ def create_graph(csv_file_name: str, pandas_agent_query: str) -> str:
     The query should be a plain english description of the data you wish to retrieve and what to do with it.
     You do not need to ask the agent to read the CSV file, it is done automatically.
     You should always ask the agent to SAVE the created graph.
-    You might consider using the show_image tool subsequent to this tool.
-    The result is returned or an error message if something went wrong."""
+    The agent does not return true if it is complete so if there is no error assume the output file has been created"""
     try:
         pandas_agent = create_csv_agent(
-            OpenAI(temperature=0),
-            os.path.join(os.getcwd(), "ai_written_files", csv_file_name),
-            verbose=True,
-        )
+                OpenAI(temperature=0),
+                os.path.join(os.getcwd(), "ai_written_files", csv_file_name),
+                verbose=True,
+            )
     except:
         return "ERROR: Something went wrong when creating the CSV agent. Check the CSV file exists!"
-
     return pandas_agent.run(pandas_agent_query)
 
 
@@ -63,16 +62,14 @@ def wikipedia_table(link):
 
 def write_to_file(file_name: str, file_content: str) -> bool:
     """Writes file_contents to a new file with the given file name.
+    Ensure that the data being saved is only what is necessary to create the graph
     Returns a boolean indicating success (True) or failure (False)"""
     try:
-        with open(
-            os.path.join(os.getcwd(), "ai_written_files", file_name), "w"
-        ) as f:
+        with open(os.path.join(os.getcwd(), "ai_written_files", file_name), "w", newline="", encoding="utf-8") as f:
             f.write(file_content)
     except:
         return False
     return True
-
 
 
 @cl.on_chat_start
@@ -101,7 +98,7 @@ def start():
         Tool(
             name="Wikipedia Table",
             func=wikipedia_table,
-            description="useful for specifically gathering tables from a wikipedia page. will need to call the function with the link. data retrieved will need to be trimmed. Accepts a single string as input.",
+            description="useful for specifically gathering tables from a wikipedia page. will need to call the function with the link to the wikipedia page you would like to get the table from. Accepts a single string as input.",
         ),
         StructuredTool.from_function(create_graph),
         StructuredTool.from_function(write_to_file),
